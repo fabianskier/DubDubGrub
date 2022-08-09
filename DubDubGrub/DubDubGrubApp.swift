6fa8df7e0fa870b1b7fcaab42f469a3895945885
Oscar Cristaldo <fabianskier@icloud.com>
2022-08-09 19:36:27 -0400

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Oscar Cristaldo on 2022-08-09.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
