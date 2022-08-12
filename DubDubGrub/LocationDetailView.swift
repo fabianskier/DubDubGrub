6a6b0a70cb7ff8df3fe694805ebe9034e9329494
Oscar Cristaldo <fabianskier@icloud.com>
2022-08-11 21:00:49 -0400

    CO-0003: Add navigation link and scrollview
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- DubDubGrub/LocationDetailView.swift
+++ DubDubGrub/LocationDetailView.swift
@@ -14,76 +14,76 @@ struct LocationDetailView: View {
                    GridItem(.flexible())]
     
     var body: some View {
-        NavigationView {
-            VStack(spacing: 16) {
-                Image("default-banner-asset")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 120)
-                HStack {
-                    Label("983 Dr. Ricardo Odriozola", systemImage: "mappin.and.ellipse")
-                        .font(.caption)
-                    
-                    Spacer()
-                }
+        VStack(spacing: 16) {
+            Image("default-banner-asset")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 120)
+            HStack {
+                Label("983 Dr. Ricardo Odriozola", systemImage: "mappin.and.ellipse")
+                    .font(.caption)
+                
+                Spacer()
+            }
+            .navigationTitle("Location Name")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.horizontal)
+            
+            Text("Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.")
+                .lineLimit(3)
+                .minimumScaleFactor(0.75)
                 .padding(.horizontal)
+            
+            ZStack {
+                Capsule()
+                    .frame(height: 80)
+                    .foregroundColor(Color(uiColor: .secondarySystemBackground))
                 
-                Text("Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.")
-                    .lineLimit(3)
-                    .minimumScaleFactor(0.75)
-                    .padding(.horizontal)
-                
-                ZStack {
-                    Capsule()
-                        .frame(height: 80)
-                        .foregroundColor(Color(uiColor: .secondarySystemBackground))
-                    
-                    HStack(spacing: 20) {
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: Color("AccentColor"), imageName: "location.fill")
-                        }
-                        Link(destination: URL(string: "https://www.apple.com")!, label: {
-                            LocationActionButton(color: Color("AccentColor"), imageName: "network")
-                        })
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: Color("AccentColor"), imageName: "phone.fill")
-                        }
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .red, imageName: "person.fill.checkmark")
-                        }
+                HStack(spacing: 20) {
+                    Button {
+                        
+                    } label: {
+                        LocationActionButton(color: Color("AccentColor"), imageName: "location.fill")
+                    }
+                    Link(destination: URL(string: "https://www.apple.com")!, label: {
+                        LocationActionButton(color: Color("AccentColor"), imageName: "network")
+                    })
+                    Button {
+                        
+                    } label: {
+                        LocationActionButton(color: Color("AccentColor"), imageName: "phone.fill")
+                    }
+                    Button {
+                        
+                    } label: {
+                        LocationActionButton(color: .red, imageName: "person.fill.checkmark")
                     }
                 }
-                .padding(.horizontal)
-                
-                Text("Who's Here?")
-                    .bold()
-                    .font(.title2)
-                
+            }
+            .padding(.horizontal)
+            
+            Text("Who's Here?")
+                .bold()
+                .font(.title2)
+            
+            ScrollView {
                 LazyVGrid(columns: columns) {
                     FirstNameAvatarView(firstName: "Oscar")
                     FirstNameAvatarView(firstName: "Fabian")
                     FirstNameAvatarView(firstName: "Cristaldo")
                     FirstNameAvatarView(firstName: "Guzman")
                 }
-                
-                Spacer()
             }
-            .navigationTitle("Location Name")
-            .navigationBarTitleDisplayMode(.inline)
+            
         }
-        
     }
 }
 
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationDetailView()
+        NavigationView {
+            LocationDetailView()
+        }
     }
 }
 
