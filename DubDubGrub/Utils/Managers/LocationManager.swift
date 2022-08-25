7864d2ad4ad252d46043be87e3f4d88fc70286c7
fabianskier <fabianskier@icloud.com>
2022-08-24 21:15:18 -0400

    CO-0009: Add locations EnvironmentObject & Map Pins
    Signed-off-by: fabianskier <fabianskier@icloud.com>

---
+++ DubDubGrub/Utils/Managers/LocationManager.swift
@@ -0,0 +1,12 @@
+//
+//  LocationManager.swift
+//  DubDubGrub
+//
+//  Created by Oscar Cristaldo on 2022-08-24.
+//
+
+import Foundation
+
+final class LocationManager: ObservableObject {
+    @Published var locations: [DDGLocation] = []
+}
